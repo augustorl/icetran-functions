cd2f00bd5828dac130b4d8cf4e1a047b6bfe89cb
augustorl <gutin22@gmail.com>
2021-08-02 17:02:16 -0300

    add: cors to index

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,18 +3,27 @@ import * as admin from 'firebase-admin';
 import * as firestore from "firebase-admin";
 import firebase from "firebase";
 import { firebaseConfig } from "./config/firebase";
-import { createUser, deleteUser, getUser, getUsers } from "./handlers/users";
+import { createUser, deleteUser, getUser, getUsers, updateUser } from "./handlers/users";
+const cors = require('cors');
+
 const app = require('express')();
+
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
 
 
 // Initialize firebase
 firebase.initializeApp(firebaseConfig);
 var auth = firebase.auth();
 auth.useEmulator("http://localhost:9099")
-admin.initializeApp();
+admin.initializeApp({
+    serviceAccountId: 'firebase-adminsdk-9faid@icetran-vendas-bdcba.iam.gserviceaccount.com',
+});
+  
 export const db = firestore.firestore();
 db.settings({ ignoreUndefinedProperties: true });
-
 
 
 
@@ -22,6 +31,7 @@ db.settings({ ignoreUndefinedProperties: true });
 app.get('/users', getUsers)
 app.get('/user/:id', getUser);
 app.post('/user', createUser);
+app.patch('/user/:id', updateUser);
 app.delete('/user/:id', deleteUser);
 
 exports.api = functions.https.onRequest(app);
